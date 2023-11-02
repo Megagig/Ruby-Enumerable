b9de4bc6b7aa85f31054ee8c6a702820a0e66957
megagig <megagigsoftwaresolutions@gmail.com>
2023-11-02 17:36:01 +0100

    Create list

---
+++ my_list.rb
@@ -0,0 +1,22 @@
+require_relative 'my_enumerable'
+
+class MyList
+  include MyEnumerable
+
+  def initialize(*list)
+    @list = list
+  end
+end
+
+# Usage example:
+list = MyList.new(1, 2, 3, 4)
+
+# Test #all?
+puts list.all? { |e| e < 5 } #=> true
+puts list.all? { |e| e > 5 } #=> false
+
+puts list.any? { |e| e == 2 } #=> true
+puts list.any? { |e| e == 5 } #=> false
+
+# Test #filter
+p list.filter(&:even?) #=> [2, 4]
